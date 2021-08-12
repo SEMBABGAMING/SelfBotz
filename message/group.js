8db344c4e7106b7d7bc49280d319b44b0b78ee1a
Nino chan <81684610+Nino-chan02@users.noreply.github.com>
2021-08-13 00:32:55 +0700

    Add files via upload

--- message/group.js
+++ message/group.js
@@ -2,7 +2,6 @@ const {
 	MessageType
 } = require("@adiwajshing/baileys");
 const fs = require("fs-extra")
-const moment = require("moment-timezone")
 
 const { getBuffer } = require('../lib/myfunc')
 const { color, bgcolor } = require('../lib/color')
@@ -11,7 +10,7 @@ let setting = JSON.parse(fs.readFileSync('./setting.json'))
 prefix = setting.prefix
 
 module.exports = welcome = async (nino, anu) => {
-	    const welkom = JSON.parse(fs.readFileSync('./database/welcome.json'))
+	    const welkom = JSON.parse(fs.readFileSync('./database/group/welcome.json'))
 	    const isWelcome = welkom.includes(anu.jid)
 	    if (!isWelcome) return
 		try {
@@ -25,10 +24,10 @@ module.exports = welcome = async (nino, anu) => {
                 try {
                 pp_grup = await nino.getProfilePicture(anu.jid)
                 } catch (e) {
-                pp_grup = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60'
+                pp_grup = 'https://i.postimg.cc/SN54m6LW/SAVE-20210728-133334.jpg'
             }
             if (anu.action == 'add' && mem.includes(nino.user.jid)) {
-            nino.sendMessage(anu.jid, `Halo! Terima Kasih sudah Mengundangku, Jika ingin Menggunakan Bot Ketik ${prefix}menu`, 'conversation')
+            nino.sendMessage(anu.jid, 'Halo! Terima Kasih sudah Mengundangku, Jika ingin Menggunakan Bot Ketik !menu', 'conversation')
             }
              if (anu.action == 'add' && !mem.includes(nino.user.jid)) {
              if (!welkom.includes(anu.jid)) return
@@ -37,22 +36,28 @@ module.exports = welcome = async (nino, anu) => {
             	num = anu.participants[0]
                 let v = nino.contacts[num] || { notify: num.replace(/@.+/, '') }
                 anu_user = v.vname || v.notify || num.split('@')[0]
-                time_wel = moment.tz('Asia/Jakarta').format("HH:mm")
-                teks = `Halo ${anu_user} \n\nNama : \nUmur :\nGender : \nAsal :\n\nSemoga Betah dan jangan lupa isi\nAnd Following Rules Group\n${mdata.desc}`
-	            buff = await getBuffer(`https://hardianto-chan.herokuapp.com/api/tools/welcomer2?name=${encodeURI(anu_user)}&descriminator=${time_wel}&totalmem=${memeg}&namegb=${encodeURI(mdata.subject)}&ppuser=${pp_user}&background=https://telegra.ph/file/9c41931cada0cfe68b8b7.jpg&apikey=hardianto`)
-		        nino.sendMessage(mdata.id, buff, MessageType.image, {caption: teks, contextInfo: {"mentionedJid": [num]}})
-		}
+                teks = `Halo ${anu_user} \n\nNama : \nUmur :\nGender : \nAsal :\n\nSemoga Betah dan jangan lupa isi\nAnd Following Rules Group`
+	            buff = await getBuffer(`http://hadi-api.herokuapp.com/api/card/welcome?nama=${anu_user}&descriminator=${memeg}&memcount=${memeg}&gcname=${encodeURI(mdata.subject)}&pp=${pp_user}&bg=https://i.postimg.cc/rFkw8MpX/IMG-20210807-151325.jpg`)
+                buttons = [{buttonId: ``,buttonText:{displayText: 'Welcome👋'},type:1}]
+                imageMsg = (await nino.prepareMessageMedia((buff), 'imageMessage', {thumbnail: buff})).imageMessage
+                buttonsMessage = { contentText: `${teks}`, footerText: '', imageMessage: imageMsg, buttons: buttons, headerType: 4 }
+                prep = await nino.prepareMessageFromContent(mdata.id,{buttonsMessage},{})
+                nino.relayWAMessage(prep)
+}
             if (anu.action == 'remove' && !mem.includes(nino.user.jid)) {
             if (!welkom.includes(anu.jid)) return
                 mdata = await nino.groupMetadata(anu.jid)
             	num = anu.participants[0]
                 let w = nino.contacts[num] || { notify: num.replace(/@.+/, '') }
                 anu_user = w.vname || w.notify || num.split('@')[0]
-                time_wel = moment.tz('Asia/Jakarta').format("HH:mm")
                 memeg = mdata.participants.length
                 out = `Kenapa tuh? kok bisa keluar? \nSayonara ${anu_user} we will miss you`
-                buff = await getBuffer(`https://hardianto-chan.herokuapp.com/api/tools/leave2?name=${encodeURI(anu_user)}&descriminator=${time_wel}&totalmem=${memeg}&namegb=${encodeURI(mdata.subject)}&ppuser=${pp_user}&background=https://telegra.ph/file/9c41931cada0cfe68b8b7.jpg&apikey=hardianto`)
-                nino.sendMessage(mdata.id, buff, MessageType.image, {caption: out, contextInfo: {"mentionedJid": [num]}})
+                buff = await getBuffer(`http://hadi-api.herokuapp.com/api/card/goodbye?nama=${anu_user}&descriminator=${memeg}&memcount=${memeg}&gcname=${encodeURI(mdata.subject)}&pp=${pp_user}&bg=https://i.postimg.cc/rFkw8MpX/IMG-20210807-151325.jpg`)
+                buttons = [{buttonId: ``,buttonText:{displayText: 'Sayonara👋'},type:1}]
+                imageMsg = (await nino.prepareMessageMedia((buff), 'imageMessage', {thumbnail: buff})).imageMessage
+                buttonsMessage = { contentText: `${out}`, footerText: '', imageMessage: imageMsg, buttons: buttons, headerType: 4 }
+                prep = await nino.prepareMessageFromContent(mdata.id,{buttonsMessage},{})
+                nino.relayWAMessage(prep)
             }
 		} catch (e) {
 			console.log('Error : %s', color(e, 'red'))
